9cbc6507be90c649bcf032ab9761a72586c73d0d
Kamila Duda <kakus86@o2.pl>
2020-05-21 11:26:43 +0200

    Mobile keyboard touch event

--- script.js
+++ script.js
@@ -4,9 +4,53 @@ const restart__button = document.querySelector(".js-restart__button");
 const no = new Audio("no.wav");
 const loss = new Audio("przegrana.wav");
 const win = new Audio("wygrana.mp3");
-const hidden = document.getElementById("hidden");
 
 
+if(window.matchMedia('(max-device-width: 960px)').matches){
+    const container = document.querySelector(".container");
+    let input = document.createElement("INPUT");
+    input.setAttribute('type', 'text');
+    input.className="hidden";
+    input.focus();
+    container.appendChild(input);
+    if (startGame == true) {
+        let ascii = document.getElementById("litera").innerHTML;
+        let inputLetter = document.querySelector(".hidden").innerHTML;
+        inputLetter.style.textTransform="uppercase";
+        let good = parseInt(document.getElementById("good").innerHTML);
+        let bad = parseInt(document.getElementById("bad").innerHTML);
+        if(ascii==inputLetter){
+            good += 1;
+            let height = good * 5;
+            document.getElementById("good").innerHTML = good;
+            document.querySelector(".grow-green").style.height = height + "%";
+
+            if (good > 19) {
+                let time = document.getElementById("timer").innerHTML;
+                document.querySelector(".game__end--win").style.display = "unset";
+                document.querySelector(".game__time").innerHTML = time;
+                clearInterval(odliczanie);
+                win.play();
+                startGame = false;
+            }
+        } else {
+            no.play();
+            bad += 1;
+            let height = bad * 20;
+            document.getElementById("bad").innerHTML = bad;
+            document.querySelector(".grow-red").style.height = height + "%";
+            if (bad > 4) {
+                document.querySelector(".game__end--loss").style.display = "unset";
+                clearInterval(odliczanie);
+                loss.play();
+                startGame = false;
+            }
+        }
+        losuj_litere();
+        };
+
+    }else{
+   // alert("ok");
 document.addEventListener('keypress', function (e) {
     if (startGame == true) {
         let clickLetter = e.key.charCodeAt(0);
@@ -45,6 +89,7 @@ document.addEventListener('keypress', function (e) {
         losuj_litere();
     }
 });
+};
 
 function timer() {
     let timer = document.getElementById("timer");
@@ -78,7 +123,7 @@ start__button.addEventListener("click", function () {
     losuj_litere();
     start__button.style.display = "none";
     restart__button.style.display = "unset";
-    hidden.focus();
+  
     
 });
 restart__button.addEventListener("click", function () {
@@ -97,10 +142,6 @@ document.getElementById("winClose").addEventListener("click", function () {
     startGame = false;
     beginning();
 });
-/*if(window.matchMedia('(max-device-width: 960px)').matches){
-   // start__button.focus();
-   alert("ok");
-   
-};*/
+
 
 
